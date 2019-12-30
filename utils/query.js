9f10e4caba9a3a700552d56c71cb59431a374a93
IceSword8492 <cleared.for.approach.runway23@gmail.com>
2019-12-30 11:31:06 +0900

    updated

--- utils/query.js
+++ utils/query.js
@@ -1,7 +1,4 @@
 const rp = require("request-promise");
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const Types = {
     keyword: 0,
@@ -181,8 +178,7 @@ module.exports = Query = class Query {
         let sql;
         switch (mode) {
         case 1:
-            console.log(process.env.DBNAME, `${where.length ? "having" : ""} ${where.join(" and ")}`);
-            sql = `select count(t.uuid) as count from (select uuid, end - unix_timestamp(now()) * 1000 as time, greatest(highest_bid_amount, starting_bid) as price from ${process.env.DBNAME}.auctions ${where.length ? "having" : ""} ${where.join(" and ")}) as t`;
+            sql = `select count(t.uuid) as count from (select uuid, end - unix_timestamp(now()) * 1000 as time, greatest(highest_bid_amount, starting_bid) as price from auctions ${where.length ? "having" : ""} ${where.join(" and ")}) as t`;
             break;
         case 0:
         default:
