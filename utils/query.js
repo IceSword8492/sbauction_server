c6bdc1f61a2722fe282c59f52e3d69f346813e77
IceSword8492 <cleared.for.approach.runway23@gmail.com>
2019-12-30 11:16:55 +0900

    updated

--- utils/query.js
+++ utils/query.js
@@ -178,7 +178,7 @@ module.exports = Query = class Query {
         let sql;
         switch (mode) {
         case 1:
-            sql = `select count(uuid) as count from (select uuid, end - unix_timestamp(now()) * 1000 as time, greatest(highest_bid_amount, starting_bid) as price from auctions ${where.length ? "where" : ""} ${where.join(" and ")})`;
+            sql = `select count(t.uuid) as count from (select uuid, end - unix_timestamp(now()) * 1000 as time, greatest(highest_bid_amount, starting_bid) as price from auctions ${where.length ? "having" : ""} ${where.join(" and ")}) as t`;
             break;
         case 0:
         default:
